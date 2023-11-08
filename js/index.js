faa8d9bde5d3ac94d18714d7757bd14e6574ccde
FranchuDev <franchiko1@gmail.com>
2023-11-08 00:00:59 -0300

    carga de variables de seleecion de mascota

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
+function iniciarJuego() {
+	let botonMascotaJugador = document.getElementById("boton-mascota");
+	botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+}
+
 function seleccionarMascotaJugador() {
+	let inputHypodoge = document.getElementById("Hypodoge");
+	let inputKapypepo = document.getElementById("Kapypepo");
+	let inputRatyheia = document.getElementById("Ratyheia");
+
+	if (inputHypodoge.checked) {
+		alert("Seleccionaste a Hypodoge");
+	} else if (inputKapypepo.checked) {
+		alert("Seleccionaste a Kapypepo");
+	} else if (inputRatyheia.checked) {
+		alert("Seleccionaste a Ratyheia");
+	} else {
+		alert("Selecciona una mascota");
+	}
+
 	alert("Eligieron un Digibeast ya ! \n\nA JUGAR!!");
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota");
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+// Este codigo hace que se cargue el archivo JS al momento de cargar la pagina
+window.addEventListener("load", iniciarJuego);
