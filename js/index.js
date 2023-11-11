a7613e92695f8ae1b0ed621333eae2714965d2b1
FranchuDev <franchiko1@gmail.com>
2023-11-10 23:44:13 -0300

    load de funciones de ataques aleatorios de enemigos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
+let ataqueJugador;
+let ataqueEnemigo;
+
 function iniciarJuego() {
 	let botonMascotaJugador = document.getElementById("boton-mascota");
 	botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+
+	//Selector de tipos de ataque de Juagador
+	let botonFuego = document.getElementById("boton-fuego");
+	botonFuego.addEventListener("click", ataqueFuego);
+	let botonAgua = document.getElementById("boton-agua");
+	botonAgua.addEventListener("click", ataqueAgua);
+	let botonTierra = document.getElementById("boton-tierra");
+	botonTierra.addEventListener("click", ataqueTierra);
 }
 
 function seleccionarMascotaJugador() {
@@ -21,22 +32,50 @@ function seleccionarMascotaJugador() {
 	seleccionarMascotaEnemigo();
 
 	alert("Ya elegiste un Digibeast \n\nA JUGAR!!");
+}
 
-	function seleccionarMascotaEnemigo() {
-		let ataqueAleatorio = aleatorio(1, 3);
-		let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
-		if (ataqueAleatorio == 1) {
-			spanMascotaEnemigo.innerHTML = "Hypodoge";
-		} else if (ataqueAleatorio == 2) {
-			spanMascotaEnemigo.innerHTML = "Kapypeppo";
-		} else {
-			spanMascotaEnemigo.innerHTML = "Ratyheia";
-		}
+function seleccionarMascotaEnemigo() {
+	let mascotaAleatorio = aleatorio(1, 3);
+	let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+	if (mascotaAleatorio == 1) {
+		spanMascotaEnemigo.innerHTML = "Hypodoge";
+	} else if (mascotaAleatorio == 2) {
+		spanMascotaEnemigo.innerHTML = "Kapypeppo";
+	} else {
+		spanMascotaEnemigo.innerHTML = "Ratyheia";
 	}
+}
 
-	function aleatorio(min, max) {
-		return Math.floor(Math.random() * (max - min + 1) + min);
+//Funciones para cargar el tipo de ataque de jugador
+function ataqueFuego() {
+	ataqueJugador = "FUEGO";
+	ataqueAleatorioEnemigo();
+}
+
+function ataqueAgua() {
+	ataqueJugador = "AGUA";
+	ataqueAleatorioEnemigo();
+}
+
+function ataqueTierra() {
+	ataqueJugador = "TIERRA";
+	ataqueAleatorioEnemigo();
+}
+
+function ataqueAleatorioEnemigo() {
+	let ataqueAleatorio = aleatorio(1, 3);
+
+	if (ataqueAleatorio == 1) {
+		ataqueEnemigo = "FUEGO";
+	} else if (ataqueAleatorio == 2) {
+		ataqueEnemigo = "AGUA";
+	} else {
+		ataqueEnemigo = "TIERRA";
 	}
+}
+
+function aleatorio(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Este codigo hace que se cargue el archivo JS al momento de cargar la pagina
